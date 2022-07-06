006fca527d5bc165d2249d1d842f418c8778103d
Marko Toplak <marko.toplak@gmail.com>
2022-07-06 14:48:56 +0200

    DaskTable: in-memory metas

--- Orange/data/dask.py
+++ Orange/data/dask.py
@@ -29,13 +29,15 @@ class DaskTable(Table):
         if 'W' in f:
             self.W = da.from_array(f['W'])
         else:
-            self.W = self.metas = np.ones((len(X), 0))
-
-        # TODO ids need to be set
-
+            self.W = np.ones((len(X), 0))
         self.X = da.from_array(X)
         self.Y = da.from_array(Y)
-        self.metas = np.ones((len(self.X), 0))  # TODO
+
+        # TODO for now, metas are in memory
+        if "metas" in f:
+            self.metas = pickle.loads(np.array(f['metas']).tobytes())
+        else:
+            self.metas = np.ones((len(self.X), 0))
 
         self.domain = pickle.loads(np.array(f['domain']).tobytes())
 
@@ -59,9 +61,8 @@ def table_to_dask(table, filename):
     with h5py.File(filename, 'w') as f:
         f.create_dataset("X", data=table.X)
         f.create_dataset("Y", data=table.Y)
-        domain = Domain(table.domain.attributes, table.domain.class_vars, None)
-        f.create_dataset("domain", data=np.void(pickle.dumps(domain)))
-        #f.create_dataset("metas", data=table.metas)  # TODO object
+        f.create_dataset("domain", data=np.void(pickle.dumps(table.domain)))
+        f.create_dataset("metas", data=np.void(pickle.dumps(table.metas)))
 
 
 if __name__ == '__main__':
@@ -69,24 +70,29 @@ if __name__ == '__main__':
     # table_to_dask(iris, "iris.hdf5")
     # dt = DaskTable.from_file("iris.hdf5")
 
-    # zoo = Table("zoo.tab")
-    # table_to_dask(zoo, "zoo.hdf5")
+    zoo = Table("zoo.tab")
+    table_to_dask(zoo, "zoo.hdf5")
 
+    t = DaskTable.from_file("zoo.hdf5")
+    print(t.domain)
+    print(t.metas)
 
     def variable(x):
         if x < 10000:
             return ContinuousVariable("Continuous " + str(x))
         return DiscreteVariable("Discrete " + str(x), ["1", "2", "3", "4"])
 
-    domain = Domain([variable(i) for i in range(20000)])
-    x = np.c_[np.random.random((20000, 10000)), np.random.randint(4, size=(20000, 10000))]
-    bigtable = Table.from_numpy(domain=domain, X=x)
-    # print(bigtable.X.shape)
-    table_to_dask(bigtable, "t4e6_mixed.hdf5")
+    if 0:
 
-    # print(iris[0])
-    # print(dt[0])
+        domain = Domain([variable(i) for i in range(20000)])
+        x = np.c_[np.random.random((20000, 10000)), np.random.randint(4, size=(20000, 10000))]
+        bigtable = Table.from_numpy(domain=domain, X=x)
+        # print(bigtable.X.shape)
+        table_to_dask(bigtable, "t4e6_mixed.hdf5")
 
-    # print(dt.X)
-    # print(dt)
+        # print(iris[0])
+        # print(dt[0])
+
+        # print(dt.X)
+        # print(dt)
 
