9aec6bbafea934d6b2e91dde01ecd5f064e028b5
Marko Toplak <marko.toplak@gmail.com>
2022-07-21 12:22:27 +0200

    Fix dask_stats for 1D tables

--- Orange/tests/test_dasktable.py
+++ Orange/tests/test_dasktable.py
@@ -1,11 +1,23 @@
 import unittest
+from contextlib import contextmanager
 
 import numpy.testing
 import dask.array as da
 
 from Orange.data import Table, Domain
-from Orange.data.dask import DaskTable
+from Orange.data.dask import DaskTable, dask_stats
+from Orange.statistics.basic_stats import DomainBasicStats
 from Orange.tests import named_file
+
+
+@contextmanager
+def open_as_dask(table):
+    if isinstance(table, str):
+        table = Table(table)
+    with named_file('') as fn:
+        DaskTable.save(table, fn)
+        dasktable = DaskTable.from_file(fn)
+        yield dasktable
 
 
 class TableTestCase(unittest.TestCase):
@@ -46,3 +58,40 @@ class TableTestCase(unittest.TestCase):
                 dzoo.save(fn2)
                 dzoo2 = DaskTable.from_file(fn)
                 self.same_tables(zoo, dzoo2)
+
+    def test_dask_stats(self):
+        with open_as_dask("zoo") as data:
+            stats = dask_stats(data.X)
+            self.assertIsInstance(stats, da.Array)
+            s = stats.compute()
+            self.assertEqual(s.shape, (data.X.shape[1], 6))
+            numpy.testing.assert_almost_equal(s[0],
+                                              [0., 1., 0.4257426, 0., 0., 101.])
+            stats = dask_stats(data.X, compute_variance=True)
+            s = stats.compute()
+            numpy.testing.assert_almost_equal(s[0],
+                                              [0., 1., 0.4257426, 0.2444858, 0., 101.])
+
+    def test_dask_stats_1d_matrix(self):
+        with open_as_dask("zoo") as data:
+            stats = dask_stats(data.Y)
+            self.assertIsInstance(stats, da.Array)
+            s = stats.compute()
+            self.assertEqual(s.shape, (1, 6))
+            numpy.testing.assert_almost_equal(s[0],
+                                              [0., 6., 3.41584158, 0., 0., 101.])
+
+    def test_domain_basic_stats(self):
+        with open_as_dask("housing") as data:
+            print(len(data.domain))
+            stats = DomainBasicStats(data, compute_variance=True)
+            f = stats[0]
+            self.assertEqual(f.min, 0.00632)
+            self.assertEqual(f.max, 88.9762)
+            self.assertAlmostEqual(f.mean, 3.6135236)
+            self.assertAlmostEqual(f.var, 73.8403597)
+            c = stats[13]  # class value
+            self.assertEqual(c.min, 5)
+            self.assertEqual(c.max, 50)
+            self.assertAlmostEqual(c.mean, 22.5328063)
+            self.assertAlmostEqual(c.var, 84.4195562)
