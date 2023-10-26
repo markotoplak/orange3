91a31a59521624bd65936c8a82b480c2540e453b
Marko Toplak <marko.toplak@gmail.com>
2023-10-26 14:10:35 +0200

    dask PCA: use incremental PCA for better memory efficieny

--- Orange/tests/test_pca.py
+++ Orange/tests/test_pca.py
@@ -159,9 +159,8 @@ class TestPCA(unittest.TestCase):
 
     @unittest.skipIf(sklearn_version.startswith('0.20'),
                      "https://github.com/scikit-learn/scikit-learn/issues/12234")
-    @with_dasktable
-    def test_incremental_pca(self, prepare_table):
-        data = prepare_table(self.ionosphere)
+    def test_incremental_pca(self):
+        data = self.ionosphere
         self.__ipca_test_helper(data, n_com=3, min_xpl_var=0.49)
         self.__ipca_test_helper(data, n_com=32, min_xpl_var=1)
 
